327bc8968d98a6921dc3e497fb14460f9e124059
JiWedetir <serafettindiyart@hotmail.com>
2023-09-05 18:50:57 +0200

    Fertige Implementierung

--- GeoShapes/Program.cs
+++ GeoShapes/Program.cs
@@ -4,39 +4,6 @@
     {
         static void Main(string[] args)
         {
-            //Linie linie = new Linie(-2);
-
-            /*
-            Linie linieK = new Linie(2);
-            Kreis kreis = new Kreis(linieK);
-            Console.WriteLine(kreis.BerechneUmfang());
-            Console.WriteLine(kreis.BerechneFlaeche());
-            */
-
-            /*
-            Linie linie1 = new Linie(2);
-            Linie linie2 = new Linie(4);
-            Linie linie3 = new Linie(6);
-            Dreieck dreieck = new Dreieck(linie1, linie2, linie3);
-            Linie linie4 = new Linie(3);
-            dreieck = new Dreieck(linie1, linie2, linie4);
-            Console.WriteLine(dreieck.BerechneUmfang());
-            Console.WriteLine(dreieck.BerechneFlaeche());
-            */
-
-            /*
-            Linie linieA = new Linie(2);
-            Linie linieB = new Linie(2);
-            Linie linieC = new Linie(4);
-            Linie linieD = new Linie(5);
-            Rechteck rechteck = new Rechteck(linieA, linieB, linieC, linieD);
-            Linie linieE = new Linie(4);
-            rechteck = new Rechteck(linieA, linieB, linieC, linieE);
-            Console.WriteLine(rechteck.BerechneUmfang());
-            Console.WriteLine(rechteck.BerechneFlaeche());
-            */
-
-            /*
             Linie linieA = new Linie(2);
             Linie linieB = new Linie(2);
             Linie linieC = new Linie(4);
@@ -44,30 +11,18 @@
             Linie linieE = new Linie(3);
             Linie linieF = new Linie(3);
 
-            Rechteck rechteckA = new Rechteck(linieA, linieB, linieC, linieD);
-            Rechteck rechteckB = new Rechteck(linieA, linieB, linieE, linieF);
-            Kreis kreis1 = new Kreis(linieA);
-
-            Formenbehaelter formenbehaelter = new Formenbehaelter(rechteckA, rechteckB, kreis1);
-            formenbehaelter.FuegeFormHinzu(kreis1);
-            formenbehaelter.FuegeFormHinzu(kreis1);
-            formenbehaelter.FuegeFormHinzu(kreis1);
-            formenbehaelter.EntferneForm(kreis1);
-            formenbehaelter.EntferneForm(kreis1);
-            formenbehaelter.EntferneForm(kreis1);
+            Rechteck rechteck1 = new Rechteck(linieA, linieB, linieC, linieD);
+            Rechteck rechteck2 = new Rechteck(linieA, linieB, linieE, linieF);
 
             Dreieck dreieck1 = new Dreieck(linieA, linieC, linieE);
+
+            Kreis kreis1 = new Kreis(linieA);
+            Kreis kreis2 = new Kreis(linieC);
+
+            Formenbehaelter formenbehaelter = new Formenbehaelter(rechteck1, rechteck2, kreis1);
             formenbehaelter.FuegeFormHinzu(dreieck1);
-            formenbehaelter.FuegeFormHinzu(dreieck1);
-            formenbehaelter.FuegeFormHinzu(dreieck1);
-            formenbehaelter.FuegeFormHinzu(dreieck1);
-            formenbehaelter.FuegeFormHinzu(dreieck1);
-            formenbehaelter.EntferneForm(dreieck1);
-            formenbehaelter.EntferneForm(dreieck1);
-            formenbehaelter.EntferneForm(dreieck1);
-            formenbehaelter.EntferneForm(dreieck1);
-            formenbehaelter.EntferneForm(dreieck1);
-            */
+            formenbehaelter.FuegeFormHinzu(kreis2);
+            
         }
     }
 }
